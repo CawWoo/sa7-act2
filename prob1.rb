fc3a1b76e2e83383884fa278e64ef05862e23125
CawWoo <melodicraven@gmail.com>
2024-02-15 17:18:40 -0600

    Solved problem 1

--- prob1.rb
+++ prob1.rb
@@ -1,4 +1,8 @@
 #Problem 1
+#implement a method tag
+def tag t, content
+  "<#{t}>#{content}</#{t}>"
+end
 
-puts tag :p, "This is a paragraph in DSL."
-puts tag :h1, "This is a header in DSL."
+puts tag :p, "This is a paragraph"
+puts tag :h1, "This is a header"
