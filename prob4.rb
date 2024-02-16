a94d796a5aee383a405fafc75687954c2e8d8a36
CawWoo <melodicraven@gmail.com>
2024-02-15 20:15:42 -0600

    Solved problem 4, and added input and output text files for problem 4

--- prob4.rb
+++ prob4.rb
@@ -1,1 +1,15 @@
 #File I/O
+
+begin
+  contents = File.read("input.txt")
+  contents.reverse!
+
+  File.open('output.txt', 'w') do |file|
+    file.write(contents)
+  end
+
+rescue => e
+  puts "Error: #{e.FileDoesNotExist}"
+ensure
+  puts "Done"
+end
